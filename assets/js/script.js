8f0d88aa4474ab9430c3aa5fd6bf3b2868d94fc0
riet <adamriet@gmail.com>
2023-03-28 17:06:43 -0600

    added javascript variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={d94fcd0a3f247519e9f2462c13c0bc86}
+var cityInputEl = document.querySelector('#searchBox');
+var cityHistoryEl = document.querySelector('#previous-searchers');
+var weatherInfoEl = document.querySelector('#weather-data');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var apiUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={}';
