ddcb850f8b03ef9587087820428e9db9e93daec5
riet <adamriet@gmail.com>
2023-03-29 12:00:21 -0600

    updated JS to collected city name in search box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,46 @@
 var cityInputEl = document.querySelector('#searchBox');
 var cityHistoryEl = document.querySelector('#previous-searchers');
 var weatherInfoEl = document.querySelector('#weather-data');
+var searchButtonEl = document.querySelector('button');
+
+
+var citySubmitHandler = function (event) {
+    event.preventDefault();
+  
+    var cityName = cityInputEl.value.trim();
+    console.log(cityName);
+    if (cityName) {
+        getUserRepos(cityName);
+
+     
+  
+      cityHistoryEl.textContent = '';
+      cityInputEl.value = '';
+    } else {
+      alert('Please enter a city');
+    }
+  };
+
+
+    var getUserWeather = function (user) {
+    var apiUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={d94fcd0a3f247519e9f2462c13c0bc86}';
+  
+    fetch(apiUrl)
+      .then(function (response) {
+        if (response.ok) {
+          console.log(response);
+          response.json().then(function (data) {
+            console.log(data);
+            displayRepos(data, user);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to GitHub');
+      });
+  };
 
 
 
@@ -22,14 +62,7 @@ var weatherInfoEl = document.querySelector('#weather-data');
 
 
 
-
-
-
-
-
-
-
-
+  searchButtonEl.addEventListener('click', citySubmitHandler);
 
 
 var apiUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={d94fcd0a3f247519e9f2462c13c0bc86}';
