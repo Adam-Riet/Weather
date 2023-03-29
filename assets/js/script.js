7476158ef68f013c5c1edeb7c636196585fd1a3a
riet <adamriet@gmail.com>
2023-03-28 18:02:08 -0600

    added back weather api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,4 +32,4 @@ var weatherInfoEl = document.querySelector('#weather-data');
 
 
 
-var apiUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={}';
+var apiUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={d94fcd0a3f247519e9f2462c13c0bc86}';
